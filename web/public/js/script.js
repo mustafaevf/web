cc8c8aa73bbeaa156d721eef6b6b5d54b7542af3
mustafaevf <knetya274@gmail.com>
2023-12-18 00:09:57 +0300

    first commit

---
+++ web/public/js/script.js
@@ -0,0 +1,61 @@
+function login() {
+    const username = $('#auth-login').val();
+    const password = $('#auth-password').val();
+  
+    $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $.ajax({
+      url: '/login', 
+      type: 'POST',
+      data: {
+        username: username,
+        password: password
+      },
+      success: function(response) {
+        if(response == 'ok') {
+          href('/');
+        }
+        console.log(response)
+        
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+        alert('Ошибка при отправке данных на сервер!');
+      }
+    });
+}
+
+function register() {
+    const username = $('#auth-login').val();
+    const email = $('#auth-email').val();
+    const password = $('#auth-password').val();
+    const rePassword = $('#auth-re-password').val();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const data = {
+      'username': username,
+      'password': password,
+      'email': email,
+      'repassword': rePassword
+    } 
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
+    $.ajax({
+      url: '/register', 
+      type: 'POST',
+      data: data,
+      success: function(response) {
+        console.log(response)
+        
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+        alert('Ошибка при отправке данных на сервер!');
+      }
+    });
+}
---
+++ web/resources/views/auth/login.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="left__container">
+            <div class="left__container-header">
+                <div class="block">
+                    <div class="block__inner">
+                        <div class="block-left">
+                            <div class="block-avatar">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGPzSYh4QjfE8boyDWvhHoLagaM5PlnMw8sYfhR1kpow&s" alt="">
+                            </div>
+                            <div class="block-info">
+                                <span class="title">ЯУЕБАН</span>
+                                <span class="info">3 745 руб.</span>
+                            </div>
+                        </div>
+                        <div class="block-right">
+                            <a href="/logout" class="btn-error">Выйти</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="left__container-menu">
+                <li>
+                    <a href="">Продать</a>
+                </li>
+            </div>
+        </div>
+        <div class="right__container">
+            <div class="title">Авторизация</div>
+            <div class="form">
+                
+                <div class="input">
+                    <img src="{{asset('images/user.png')}}" alt="">
+                    <input type="text" autocomplete="off" id="auth-username">
+                </div>
+                
+                <div class="input">
+                    <img src="{{asset('images/lock.png')}}" alt="">
+                    <input type="password" autocomplete="off" id="auth-password">
+                </div>
+                <button onclick="login()">Войти</button>
+                <div class="form__footer">
+                    <a href="/register">Регистрация</a>
+                    <a href="/recovery">Забыли пароль?</a>
+                </div>
+            </div>
+            {{-- <div class="input">
+                <img src="{{asset('images/search.png')}}" alt="">
+                <input type="text" placeholder="Введите название товара">
+                <button>Найти</button>
+            </div> --}}
+        </div>
+    </div>
+   
+</body>
+</html>
