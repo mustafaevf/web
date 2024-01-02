b4f4698237b64c6f74bbbc949fb8154c6ba6faf7
mustafaevf <knetya274@gmail.com>
2024-01-02 22:23:04 +0300

    fix route

--- web/routes/web.php
+++ web/routes/web.php
@@ -29,8 +29,12 @@ Route::get('/withdraw', function () {
     return view('withdraw');
 });
 
-Route::get('/sell', function () {
-    return view('sell');
+Route::get('/sell/{platform_title?}', function (?string $platform_title = null) {
+    return view('sell', ['platform_title' => $platform_title]);
+});
+
+Route::get('/platforms/{platform_title?}', function (?string $platform_title = null) {
+    return view('platforms', ['platform_title' => $platform_title]);
 });
 
 Route::post('login', [LoginController::class, 'store']);
