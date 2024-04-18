e0608e32c1fadca15f8abf978b2076bfea4fcf97
mustafaevf <knetya274@gmail.com>
2024-04-18 13:05:56 +0300

    sell script

---
+++ web/public/js/viewScript/sell.js
@@ -0,0 +1,43 @@
+function SwitchSteps(parent_class) {
+    str = parent_class.toString();
+    str = str.slice(-1);
+    $(".step_" + str).css("display", "none");
+    if(str != 5) {
+        $(".step_" + String(Number(str) + 1)).css("display", "block");
+    }
+};
+
+$(".box_type2").click(function() {
+    parent = $(this).parent().parent().attr("class")
+    text_inside_box = $(this).find(".main_text").text()
+    SwitchSteps(parent)
+    if(parent == "step_1") 
+    {
+        $("#result-platform-title").text(text_inside_box);
+    } 
+    else if(parent == "step_2") {
+        $("#result-category-title").text(text_inside_box);
+    }
+});
+
+$("button").click(function () {
+    parent = $(this).parent().parent().parent().attr("class");
+    if(parent = "step_3") {
+        if($("#product-title").val().length != 0) {
+            $("#result-product-title").text($("#product-title").val());
+        }
+        if($("#product-description").val().length != 0) {
+            $("#result-product-description").text($("#product-description").val());
+        }
+        if($("#product-price").val().length != 0) {
+            $("#result-product-price").text($("#product-price").val());
+        }
+    }
+    SwitchSteps(parent);
+});
+
+$(".choose_btn").click(function () {
+    parent = $(this).parent()
+    parent.find(".choose_btn").removeClass("active");
+    $(this).addClass("active");
+});
--- web/resources/views/sell.blade.php
+++ web/resources/views/sell.blade.php
@@ -1,62 +1,115 @@
 @extends('header')
 @section('main')
-    @php
-        $platform = App\Models\Platform::where('title', ucfirst($platform_title))->first();
-        
-    @endphp
-    @if (!$platform)
-        {{redirect('error', ['message' => 'нет'])}}
-    @endif
-    <div class="title" style="display: flex; align-items: center;">
-        {{$platform->title}}
-        <img src="{{asset('images/'.$platform->img)}}" alt=""> 
+<div class="steps flex">
+    <div class="step active">
+        Основная информация
     </div>
-    <div class="form">
-        <div class="form-block">
-            <p class="label">Заголовок</p>
-            <div class="input">
-                <input type="text" autocomplete="off" placeholder="" id="sell-title">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="step">
+        Дополнительная информация
+    </div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="step">
+        Подтверждение
+    </div>
+</div>
+
+<div class="step_1">
+    <div class="main_text big mt-1">Выберите платформу</div>
+    <div class="platforms flex">
+        @php
+            $platforms = App\Models\Platform::where('status', 1)->get();
+        @endphp
+        @foreach ($platforms as $platform)
+            <div class="platform box box_type2 flex">
+                <img src="{{asset('images/'.$platform->img)}}" alt="">
+                <div class="main_text">{{$platform->title}}</div>
             </div>
-        </div>
-        <div class="form-block">
-            <p class="label">Категория</p>
-            <div class="select" id="choose-category">
-                <span>Выберите</span>
-                <img src="{{asset('images/expand_down.svg')}}" alt="">
-            </div>
-            <div class="select_opened hide">
-                @php
-                    $categories =  App\Models\Category::where('platform_id', $platform->id)->get();
-                @endphp
-                <ul>
-                    @foreach ($categories as $category)
-                        <li>{{$category->name}}</li>                        
-                    @endforeach
-                </ul>
-            </div>
-            <!-- <div class="input">
-                <input type="text" autocomplete="off" placeholder="" id="sell-category">
-            </div> -->
-        </div>
-        <div class="form-block">
-            <p class="label">Описание товара</p>
-            <div class="input">
-                <textarea id="sell-description"></textarea>
-            </div>
-        </div>
-        <div class="form-block">
-            <p class="label">Информация для покупателя</p>
-            <div class="input">
-                <textarea id="sell-info"></textarea>
-            </div>
-        </div>
-        <div class="form-block">
-            <p class="label">Цена</p>
-            <div class="input">
-                <input type="text" autocomplete="off" placeholder="" id="sell-price">
-            </div>
-        </div>
-        <button onclick=sell_confirm({{$platform->id}})>Добавить</button>
+        @endforeach
+           
     </div>
 
+</div>
+<div class="step_2" style="display: none;">
+    <div class="main_text big mt-1">Выберите категорию</div>
+    <div class="platforms flex">
+            <div class="platform box box_type2 flex">
+                <div class="main_text">Аккаунт</div>
+            </div>
+    </div>
+</div>
+
+<div class="step_3" style="display: none;">
+    <div class="form box mt-1">
+        <div class="main_text middle">Основная информация</div>
+        <div class="form_col">
+            <div class="secondary_text">Краткое описание</div>
+            <div class="input">
+                <input type="text" min-length="0" max-length="100" id="product-title" placeholder="Количество символов (0-100)">
+            </div>
+            <div class="secondary_text small danger" style="display: none;">Ошибка</div>
+        </div>
+        <div class="form_col">
+            <div class="secondary_text">Подробное описание</div>
+            <textarea name="" id="product-description"></textarea>
+            <div class="secondary_text small danger" style="display: none;">Ошибка</div>
+        </div>
+        <div class="form_col">
+            <div class="secondary_text">Цена</div>
+            <div class="input">
+                <input type="number" min-number="99" max-number="10000" id="product-price" placeholder="Минимальная цена 100 ₽">
+            </div>
+            <div class="secondary_text small danger" style="display: none;">Ошибка</div>
+        </div>
+        <div class="form_col">
+            <button>Подтвердить</button>
+        </div>
+    </div>
+</div>
+
+<div class="step_4" style="display: none;">
+    <div class="form box mt-1">
+        <div class="main_text middle">Дополнительная информация</div>
+        <div class="form_col">
+            <div class="secondary_text">Происхождение аккаунта</div>
+            <div class="choose_block flex">
+                <div class="choose_btn box active">Брут</div>
+                <div class="choose_btn box">Личный</div>
+            </div>
+        </div>
+        <div class="form_col">
+            <button>Подтвердить</button>
+        </div>
+    </div>
+</div>
+
+<div class="step_5" style="display: none;">
+    <div class="form box mt-1">
+        <div class="main_text middle">Подтверждение</div>
+        <div class="form_col">
+            <div class="secondary_text">Данные для покупателя</div>
+            <textarea name="" id=""></textarea>
+        </div>
+        <div class="form_col">
+            <button>Подтвердить</button>
+        </div>
+    </div>
+</div>
+<div class="step_6" style="display: none;">
+    <input type="text" value="" id="result-platform-title">
+    <input type="text" value="" id="result-category-title">
+    <input type="text" value="" id="result-product-title">
+    <input type="text" value="" id="result-product-description">
+    <input type="text" value="" id="result-product-aditional-information">
+    <input type="text" value="" id="result-product-price">
+    <input type="text" value="" id="result-product-action">
+</div>
+
+
+
+<script src="{{asset('js/viewScript/sell.js')}}"></script>
 @stop
